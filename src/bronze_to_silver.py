5081dc597150a78bf4ef59cae48e6d40d17c5d91
Luis Fernando <luis_sanntos@outlook.com>
2025-10-14 21:21:34 -0300

    Criação dos step de consumo, transformação e carga dos dados.

--- src/bronze_to_silver.py
+++ src/bronze_to_silver.py
@@ -1,11 +1,33 @@
+# Import libs
 import pandas as pd
 
-def CheckNull(df):
-    
+def RemoveDuplicates(df, coluna):
+    df_sem_duplicadas = df.drop_duplicates(subset=[coluna])
+    return df_sem_duplicadas
 
-# Leitura do arquivo CSV
+# Leitura dos arquivos CSV
 
-df = pd.read_csv('C:\\Users\\Nando\\Desktop\\Projeto_Ziyou\\Projeto_Ziyou\\data\\bronze\\olist_products_dataset.csv')
+df_olist_orders_bronze = pd.read_csv('C:\\Users\\Nando\\Desktop\\Projeto_Ziyou\\Projeto_Ziyou\\data\\bronze\\olist_orders_dataset.csv')
 
-duplicated = df.duplicated()
-print(duplicated.filter(like='False'))
+df_olist_order_items_bronze = pd.read_csv('C:\\Users\\Nando\\Desktop\\Projeto_Ziyou\\Projeto_Ziyou\\data\\bronze\\olist_order_items_dataset.csv')
+
+df_olist_products_bronze = pd.read_csv('C:\\Users\\Nando\\Desktop\\Projeto_Ziyou\\Projeto_Ziyou\\data\\bronze\\olist_products_dataset.csv')
+
+# Validação da integridade das PKs e FKs
+
+df_olist_orders_silver = RemoveDuplicates(df_olist_orders_bronze, 'order_id')
+
+df_olist_order_items_silver = RemoveDuplicates(df_olist_order_items_bronze, 'order_id')
+
+df_olist_products_silver = RemoveDuplicates(df_olist_products_bronze, 'product_id')
+
+# Salvando os dados na camada Silver
+
+df_olist_orders_silver.to_csv("C:\\Users\\Nando\\Desktop\\Projeto_Ziyou\\Projeto_Ziyou\\data\\silver\\olist_orders_silver.csv")
+
+df_olist_order_items_silver.to_csv("C:\\Users\\Nando\\Desktop\\Projeto_Ziyou\\Projeto_Ziyou\\data\\silver\\olist_order_items_silver.csv")
+
+df_olist_products_silver.to_csv("C:\\Users\\Nando\\Desktop\\Projeto_Ziyou\\Projeto_Ziyou\\data\\silver\\olist_products_silver.csv")
+
+
+
---
+++ src/silver_to_gold.py
@@ -0,0 +1,18 @@
+#Import libs
+import pandas as pd
+
+# Leitura dos arquivos CSV
+df_olist_orders_silver = pd.read_csv("C:\\Users\\Nando\\Desktop\\Projeto_Ziyou\\Projeto_Ziyou\\data\\silver\\olist_orders_silver.csv")
+
+df_olist_order_items_silver = pd.read_csv("C:\\Users\\Nando\\Desktop\\Projeto_Ziyou\\Projeto_Ziyou\\data\\silver\\olist_order_items_silver.csv")
+
+df_olist_products_silver = pd.read_csv("C:\\Users\\Nando\\Desktop\\Projeto_Ziyou\\Projeto_Ziyou\\data\\silver\\olist_products_silver.csv")
+
+
+# Agregando as bases a tabela gold
+f_olist_order_stagin = pd.merge(df_olist_orders_silver, df_olist_order_items_silver, on='order_id', how='left')
+
+f_olist_order_gold = pd.merge(f_olist_order_stagin, df_olist_products_silver, on='product_id', how='left')
+
+# Salvando os dados agregados na camada gold
+f_olist_order_gold.to_csv("C:\Users\Nando\Desktop\Projeto_Ziyou\Projeto_Ziyou\data\gold\\olist_order_gold.csv")
